c9d39c1baa4d39d91f69dd3bf08d9faa905171b9
Matt Berg <berg@Matts-Mac-mini.local>
2020-01-08 19:21:37 -0800

    create form for post, put and delete requests in Banners

--- src/Banners/Banners.tsx
+++ src/Banners/Banners.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import moment from 'moment';
 import { parse } from 'query-string';
 import React, { useEffect, useState } from 'react';
-import { Segment, Button } from 'semantic-ui-react';
+import { Segment, Button, Input, Grid, Label, TextArea, Form } from 'semantic-ui-react';
 import './Banners.css';
 
 const Banners = ({ location }: any) => {
@@ -46,23 +46,28 @@ const Banners = ({ location }: any) => {
       </div>
     );
   };
+  const onFormValueChange = (e: any, props: any) => {
+    console.log(props.name, props.value)
+  }
+  const handlePost = (e: any, props: any) => {
+    e.preventDefault()
 
-  const handlePost = () => {
-    const objToSend = {
-      bannerid: 12121,
-      content: 'new post',
-      dateshow: '2019-09-09', 
-      datehide: '2019-09-09',
-      showeverywhere: true
-    }
-    return axios
-    .post('http://localhost:5000/api/banner/', objToSend)
-    .then(response => {
-      console.log(response)
-    })
-    .catch((err: any) => {
-      console.log(err)
-    });
+    console.log(e)
+    // const objToSend = {
+    //   bannerid: 12121,
+    //   content: 'new post',
+    //   dateshow: '2019-09-09', 
+    //   datehide: '2019-09-09',
+    //   showeverywhere: true
+    // }
+    // return axios
+    // .post('http://localhost:5000/api/banner/', objToSend)
+    // .then(response => {
+    //   console.log(response)
+    // })
+    // .catch((err: any) => {
+    //   console.log(err)
+    // });
   }
   const handlePut = () => {
     const objToUpdate = {
@@ -100,17 +105,91 @@ const Banners = ({ location }: any) => {
         <></>
       )}
       {data && data.content ? (
-        <Segment className='banner_segment' floated='right'>
-          <h4>Banners:</h4>
-          <p>ID: {data.id}</p>
-          <p>BannerID: {data.bannerid}</p>
-          <p>Content: {data.content} </p>
-          <p>Period: {moment(data.dateshow).format('YYYY/DD/MM')} ~ {moment(data.datehide).format('YYYY/MM/DD')}</p>
-          <p>Display: {data.showeverywhere ? 'show' : 'hide'}</p>
-          <Button content='POST' onClick={handlePost}/>
-          <Button content='PUT' onClick={handlePut}/>
-          <Button content='DELETE' onClick={handleDelete}/>
-
+        <Segment className='banner_container'>
+          <Form>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                <h4>Banner:</h4>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>ID:</h4>
+                </Grid.Column>
+                <Grid.Column width={3} >
+                  <Input name='id' onChange={onFormValueChange} defaultValue={data.id} />
+                </Grid.Column>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <div>{data.id}</div>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>Banner ID:</h4>
+                </Grid.Column>
+                <Grid.Column width={3} >
+                  <Input name='bannerid' fluid={true} onChange={onFormValueChange} defaultValue={data.bannerid} />
+                </Grid.Column>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <div>{data.id}</div>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>Content:</h4>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                    <TextArea defaultValue={data.content} />
+                </Grid.Column>
+                <Grid.Column width={3} verticalAlign='middle'>
+                  <div dangerouslySetInnerHTML={{ __html: data.content }}/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>Date Show:</h4>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Input fluid={true} defaultValue={moment(data.dateshow).format('YYYY/MM/DD')} />
+                </Grid.Column>
+                <Grid.Column width={3} verticalAlign='middle'>
+                 <p>{moment(data.dateshow).format('YYYY/MM/DD')} </p>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>Date Hide:</h4>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                    <Input fluid={true} defaultValue={moment(data.datehide).format('YYYY/MM/DD')} />
+                </Grid.Column>
+                <Grid.Column width={3} verticalAlign='middle'>
+                 <p>{moment(data.datehide).format('YYYY/MM/DD')} </p>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={2} verticalAlign='middle'>
+                  <h4>Show:</h4>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                    <Input type='checkbox' defaultValue={data.showeverywhere} />
+                </Grid.Column>
+                <Grid.Column>
+                  <p>{data.showeverywhere ? 'true' : 'false'}</p>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Button.Group floated='left'>
+                    <Button content='POST' onClick={handlePost}/>
+                    <Button content='PUT' onClick={handlePut}/>
+                    <Button content='DELETE' onClick={handleDelete}/>
+                  </Button.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Form>
         </Segment>
       ) : (
         <></>
